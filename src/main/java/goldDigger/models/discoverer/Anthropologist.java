1d2c1a7f2510a7263f24e82ede45e011b89fda60
Vladimir Zorov <vl.zorov@gmail.com>
2022-09-06 21:23:40 +0300

    Add changes

--- src/main/java/goldDigger/models/discoverer/Anthropologist.java
+++ src/main/java/goldDigger/models/discoverer/Anthropologist.java
@@ -1,4 +1,8 @@
 package goldDigger.models.discoverer;
 
-public class Anthropologist {
+public class Anthropologist extends BaseDiscoverer{
+    private static final double ANTHROPOLOGIST_DISCOVERER_ENERGY = 40;
+    public Anthropologist(String name) {
+        super(name, ANTHROPOLOGIST_DISCOVERER_ENERGY);
+    }
 }
